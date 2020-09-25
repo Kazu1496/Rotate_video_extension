94c550559954b3142a91cb39da50b14a3cd2cd0c
Kazumasa Ishikura <ishikura-kazumasa1@dmm.com>
2020-09-26 01:24:30 +0900

    Remove: console.log

--- rotate.js
+++ rotate.js
@@ -11,19 +11,14 @@ const prefix = 'rotate-'
 const regexp = new RegExp('^' + prefix)
 
 document.addEventListener('keydown', (event) => {
-    console.log('video', video)
     if (event.ctrlKey && event.shiftKey){
         if (event.keyCode == 37){         // 左キー
-            console.log('LEFT')
             rotate(directions.LEFT)
         } else if (event.keyCode == 39){　// 右キー
-            console.log('RIGHT')
             rotate(directions.RIGHT)
         } else if (event.keyCode == 38){　// 上キー
-            console.log('TOP')
             rotate(directions.TOP)
         } else if (event.keyCode == 40){　// 下キー
-            console.log('BOTTOM')
             rotate(directions.BOTTOM)
         }
     }
